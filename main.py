3e237f56908ad466932ef68dce294114f7331654
Sunny175 <sunnykadia421@gmail.com>
2023-03-30 17:21:43 +0530

    add login system in project

--- main.py
+++ main.py
@@ -1,70 +1,43 @@
 import pyrebase
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from 
 
 app = Flask(__name__)
+app.secret_key = "session"
 config = {
-    "apiKey": "AIzaSyDXoDR0eixnIXtaO--ve9IYNzwoQ7mu6wg",
-    "authDomain": "project-management-syste-89346.firebaseapp.com",
-    "databaseURL": "https://project-management-syste-89346-default-rtdb.firebaseio.com",
-    "projectId": "project-management-syste-89346",
-    "storageBucket": "project-management-syste-89346.appspot.com",
-    "messagingSenderId": "139850838241",
-    "appId": "1:139850838241:web:20fecdcb0197938a2407a0",
-    "measurementId": "G-W8HK76YNWM"
+    "apiKey": "AIzaSyD7w1t1naoWypN4eEZUhiZhq0l0G6xMdvk",
+    "authDomain": "projectmanagement-3e8a8.firebaseapp.com",
+    "databaseURL": "https://projectmanagement-3e8a8-default-rtdb.firebaseio.com",
+    "projectId": "projectmanagement-3e8a8",
+    "storageBucket": "projectmanagement-3e8a8.appspot.com",
+    "messagingSenderId": "398063613086",
+    "appId": "1:398063613086:web:05aa2a5bb0ccb2d821778f",
+    "measurementId": "G-DBHEPCK3YT"
 }
 
 firebase = pyrebase.initialize_app(config=config)
 auth = firebase.auth()
 database = firebase.database()
 
-person = {
-    "is_logged_in": False,
-    "name": "",
-    "email": "",
-    "uid": ""
-}
-
 
 # Login
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            session["user_email"] = email
+            return redirect(url_for("welcome"))
+        except:
+            return "failed to login"
     return render_template("login.html")
 
 
 @app.route("/welcome")
 def welcome():
-    if person['is_logged_in'] == True:
-        return render_template("welcome.html", email=person['email'], name=person['name'])
-    else:
-        return redirect(url_for("login"))
-
-
-@app.route("/result", methods=["post", "get"])
-def result():
-    if request.method == "POST":
-        result = request.form
-        email = result['email']
-        password = result['password']
-        try:
-            user = auth.sign_in_with_email_and_password(email, password)
-
-            # Insert the user data in the global person
-            global person
-            person["is_logged_in"] = True
-            person["email"] = user['email']
-            person['uid'] = user['localId']
-
-            # Get the name of the user
-            data = database.child("users").get()
-            person['name'] = data.val()[person["uid"]["name"]]
-            return redirect(url_for("welcome"))
-        except:
-            return redirect(url_for("login"))
-    else:
-        if person['is_logged_in'] == True:
-            return redirect(url_for("welcome"))
-        else:
-            return redirect(url_for("login"))
+    return render_template("welcome.html")
 
 
 if __name__ == "__main__":
